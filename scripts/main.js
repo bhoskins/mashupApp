628b6f0aae4e37644e6f56e0fb53f35bd2dacde3
Beth Hoskins <bhoskins@charter.net>
2015-02-15 20:40:03 -0500

    giphy model and button views added. Need to get them to talk to one another and render results somehow

--- scripts/main.js
+++ scripts/main.js
@@ -31,21 +31,23 @@
 
 // How to get play and embed url
 
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=anxious+nervous&api_key=dc6zaTOxFJmzC&limit=10");
-xhr.done(function(data) {
-   console.log("success got data",
- data);
- console.log("play url: ", data.data[0].images.fixed_height.url);
- console.log("embed url: ", data.data[0].embed_url);
- var gifPlayUrl = data.data[0].images.fixed_height.url;
- var gifEmbedUrl = data.data[0].embed_url;
-});
+//  Code below is golden!
+// var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=anxious+nervous&api_key=dc6zaTOxFJmzC&limit=10");
+// xhr.done(function(data) {
+//    console.log("success got data",
+//  data);
+//  console.log("play url: ", data.data[0].images.fixed_height.url);
+//  console.log("embed url: ", data.data[0].embed_url);
+//  var gifPlayUrl = data.data[0].images.fixed_height.url;
+//  var gifEmbedUrl = data.data[0].embed_url;
+// });
 
 /**********************************************
     Models
 *******************************************/
 var searchTermPre = "\?q\=";
 var searchTermFull;
+var searchTerm;
 var gifPlayUrl;
 var gifEmbedUrl;
 var giphyUrlBase = "http://api.giphy.com/v1/gifs";
@@ -53,11 +55,21 @@ var giphyApiKey = "&api_key=dc6zaTOxFJmzC";
 var giphyLimit = "&limit=10";
 var giphySearchUrl = "http://api.giphy.com/v1/gifs/search";
 
-function giphyFetchPlayUrls(searchTerm){
-  searchTermFull = searchTermPre + searchTerm;
-  var gifUrl = (('http://api.giphy.com/v1/gifs/search&api_key=dc6zaTOxFJmzC').slice(0, 35));
-  gifPlayUrl = gifUrl.concat(searchTermPre, searchTerm, giphyApiKey, giphyLimit );
-  console.log('gifPlayUrl: '  + gifPlayUrl );
+function giphyFetchUrls(sTerm){
+  searchTerm = searchTermPre + sTerm;
+  // var gifUrl = (('http://api.giphy.com/v1/gifs/search&api_key=dc6zaTOxFJmzC').slice(0, 35));
+  var gifDataUrl = giphySearchUrl.concat(searchTerm, giphyApiKey, giphyLimit );
+  var xhr = $.get(gifDataUrl);
+xhr.done(function(data) {
+   console.log("success got data",
+ data);
+  gifPlayUrl = data.data[0].images.fixed_height.url;
+  gifEmbedUrl = data.data[0].embed_url;
+  expressEmotionView.render();
+ // console.log("play url: ", data.data[0].images.fixed_height.url);
+ // console.log("embed url: ", data.data[0].embed_url);
+});
+
 
   // anxiousDataModel.get(gifEmbedUrl);
   // console.log("function giphyFetch: " + gifPlayUrl);
@@ -76,97 +88,324 @@ var EmotionDataModel = Backbone.Model.extend({
 
 
 var anxiousDataModel = new EmotionDataModel({
-  // anxiousDataModel.fetch().then(function(){
-  //   searchTerm = searchTermPre + "anxious+nervous";
-  //   anxiousDataModel.get(searchTerm + giphyApiKey + giphyLimit);
-  //   anxiousDataModel.get(gifEmbedUrl);
-  //   // anxiousDataModel.save();
 
-    fetch: function(searchTerm){
-      searchTerm = 'anxious+nervous';
-      giphyFetchPlayUrls(searchTerm);
-    }
+
   });
 
-  anxiousDataModel.fetch('anxious+nervous');
+
+  anxiousDataModel.fetch().then(giphyFetchUrls('anxious'));
+
 
 var emotionDataModel = new EmotionDataModel();
 
-
-var EmotionsCollection = Backbone.Collection.extend({
-  model: EmotionDataModel
-});
+//
+// var EmotionsCollection = Backbone.Collection.extend({
+//   model: EmotionDataModel
+// });
 
 var EmotionsView = Backbone.View.extend({
-  collection: "EmotionsCollection",
+  // collection: "EmotionsCollection",
   el: '.container',
 
 });
 
 var emotionsAll = new EmotionsView();
 
-var AnxiousView = Backbone.View.extend({
-  collection: "EmotionsCollection",
-  model: EmotionDataModel,
-  el: '.anxious',
-    events: {
-    "click": "collectData"
-  },
-  collectData: function() {
-    var self = this;
-    console.log("anxiousView func: " + this.className);
-    // console.log("hey: " + this.className);
-
-    $("." + this.className).css({
+function blueBorder(div){
+  $(div).css({
       "border-top": "1px solid blue",
       "border-right": "3px solid blue",
       "border-bottom": "3px solid blue",
       "border-left": "1px solid blue"
     });
 
-    $("." + this.className).siblings().css({
+    $(div).siblings().css({
       "border": "none"
     });
+}
+
+
+var AnxiousView = Backbone.View.extend({
+  // collection: "EmotionsCollection",
+  model: EmotionDataModel,
+  el: '.anxious',
+  events: {
+    "click": "collectData"
+  },
+  collectData: function(){
+    searchTerm = "anxious+nervous";
+    blueBorder('.anxious');
+    console.log('anxious clicked');
   }
  });
 
+var anxiousView = new AnxiousView();
+
  var BoredView = Backbone.View.extend({
-   collection: "EmotionsCollection",
+  //  collection: "EmotionsCollection",
    model: EmotionDataModel,
    el: '.bored',
    events: {
      "click": "collectData"
    },
-   collectData: function() {
-     var self = this;
-     console.log("boredView func: " + this.className);
-     // console.log("hey: " + this.className);
-
-     $("." + this.className).css({
-       "border-top": "1px solid blue",
-       "border-right": "3px solid blue",
-       "border-bottom": "3px solid blue",
-       "border-left": "1px solid blue"
-     });
-
-     $("." + this.className).siblings().css({
-       "border": "none"
-     });
-   }
+   collectData: function(){
+    searchTerm = "bored";
+    blueBorder('.bored');
+    console.log('bored clicked');
+  }
   });
+ var boredView = new BoredView();
+// anxious, bored, cold, confident
+// curious, disappointed, disgusted, exhausted
+// happy, hot, hysterical, interested
+// lonely, satisfied, surprised, thoughtful
+
+ var RageView = Backbone.View.extend({
+  //  collection: "EmotionsCollection",
+   model: EmotionDataModel,
+   el: '.rage',
+   events: {
+     "click": "collectData"
+   },
+   collectData: function(){
+    searchTerm = "rage+furious+mad";
+    blueBorder('.rage');
+    // console.log('bored clicked');
+  }
+  });
+ var rageView = new RageView();
+
+  var ConfidentView = Backbone.View.extend({
+  //  collection: "EmotionsCollection",
+   model: EmotionDataModel,
+   el: '.confident',
+   events: {
+     "click": "collectData"
+   },
+   collectData: function(){
+    searchTerm = "confident";
+    blueBorder('.confident');
+    // console.log('bored clicked');
+  }
+  });
+ var confidentView = new ConfidentView();
+
+  var CuriousView = Backbone.View.extend({
+  //  collection: "EmotionsCollection",
+   model: EmotionDataModel,
+   el: '.curious',
+   events: {
+     "click": "collectData"
+   },
+   collectData: function(){
+    searchTerm = "curious";
+    blueBorder('.curious');
+    // console.log('bored clicked');
+  }
+  });
+ var curiousView = new CuriousView();
+
+  var DisappointedView = Backbone.View.extend({
+  //  collection: "EmotionsCollection",
+   model: EmotionDataModel,
+   el: '.disappointed',
+   events: {
+     "click": "collectData"
+   },
+   collectData: function(){
+    searchTerm = "disappointed";
+    blueBorder('.disappointed');
+    // console.log('bored clicked');
+  }
+  });
+ var disappointedView = new DisappointedView();
+// anxious, bored, cold, confident
+// curious, disappointed, disgusted, exhausted
+// happy, hot, hysterical, interested
+// lonely, satisfied, surprised, thoughtful
+
+  var DisgustedView = Backbone.View.extend({
+  //  collection: "EmotionsCollection",
+   model: EmotionDataModel,
+   el: '.disgusted',
+   events: {
+     "click": "collectData"
+   },
+   collectData: function(){
+    searchTerm = "disgusted";
+    blueBorder('.disgusted');
+    // console.log('bored clicked');
+  }
+  });
+ var disgustedView = new DisgustedView();
+
+   var ExhaustedView = Backbone.View.extend({
+  //  collection: "EmotionsCollection",
+   model: EmotionDataModel,
+   el: '.exhausted',
+   events: {
+     "click": "collectData"
+   },
+   collectData: function(){
+    searchTerm = "exhausted+tired";
+    blueBorder('.exhausted');
+    // console.log('bored clicked');
+  }
+  });
+ var exhaustedView = new ExhaustedView();
+
+  var HappyView = Backbone.View.extend({
+  //  collection: "EmotionsCollection",
+   model: EmotionDataModel,
+   el: '.happy',
+   events: {
+     "click": "collectData"
+   },
+   collectData: function(){
+    searchTerm = "happy";
+    blueBorder('.happy');
+    // console.log('bored clicked');
+  }
+  });
+ var happyView = new HappyView();
+
+  var ScaredView = Backbone.View.extend({
+  //  collection: "EmotionsCollection",
+   model: EmotionDataModel,
+   el: '.scared',
+   events: {
+     "click": "collectData"
+   },
+   collectData: function(){
+    searchTerm = "scared+hot+terrified";
+    blueBorder('.scared');
+    // console.log('bored clicked');
+  }
+  });
+ var scaredView = new ScaredView();
+// anxious, bored, cold, confident
+// curious, disappointed, disgusted, exhausted
+// happy, hot, hysterical, interested
+// lonely, satisfied, surprised, thoughtful
+
+var ExpressButtonView = Backbone.View.extend({
+  model: EmotionDataModel,
+  el: '.express-yourself',
+  events: {
+    "click": "sendData",
+    "submit": "sendData"
+  },
+  sendData: function(e){
+    e.preventDefault();
+    console.log('i need to poke model to do something');    
+  }
+});
+   var HystericalView = Backbone.View.extend({
+  //  collection: "EmotionsCollection",
+   model: EmotionDataModel,
+   el: '.hysterical',
+   events: {
+     "click": "collectData"
+   },
+   collectData: function(){
+    searchTerm = "hysterical+hilarious+funny";
+    blueBorder('.hysterical');
+    // console.log('bored clicked');
+  }
+  });
+ var hystericalView = new HystericalView();
+
+  var InterestedView = Backbone.View.extend({
+  //  collection: "EmotionsCollection",
+   model: EmotionDataModel,
+   el: '.interested',
+   events: {
+     "click": "collectData"
+   },
+   collectData: function(){
+    searchTerm = "interested";
+    blueBorder('.interested');
+    // console.log('bored clicked');
+  }
+  });
+ var interestedView = new InterestedView();
+
+   var LonelyView = Backbone.View.extend({
+  //  collection: "EmotionsCollection",
+   model: EmotionDataModel,
+   el: '.lonely',
+   events: {
+     "click": "collectData"
+   },
+   collectData: function(){
+    searchTerm = "lonely";
+    blueBorder('.lonely');
+    // console.log('bored clicked');
+  }
+  });
+ var lonelyView = new LonelyView();
+
+   var SatisfiedView = Backbone.View.extend({
+  //  collection: "EmotionsCollection",
+   model: EmotionDataModel,
+   el: '.satisfied',
+   events: {
+     "click": "collectData"
+   },
+   collectData: function(){
+    searchTerm = "satisfied";
+    blueBorder('.satisfied');
+    // console.log('bored clicked');
+  }
+  });
+ var satisfiedView = new SatisfiedView();
+
+  var SurprisedView = Backbone.View.extend({
+  //  collection: "EmotionsCollection",
+   model: EmotionDataModel,
+   el: '.surprised',
+   events: {
+     "click": "collectData"
+   },
+   collectData: function(){
+    searchTerm = "surprised+shocked";
+    blueBorder('.surprised');
+    // console.log('bored clicked');
+  }
+  });
+ var surprisedView = new SurprisedView();
+
+   var ThoughtfulView = Backbone.View.extend({
+  //  collection: "EmotionsCollection",
+   model: EmotionDataModel,
+   el: '.thoughtful',
+   events: {
+     "click": "collectData"
+   },
+   collectData: function(){
+    searchTerm = "thoughtful+thinking+ponder";
+    blueBorder('.thoughtful');
+    // console.log('bored clicked');
+  }
+  });
+ var thoughtfulView = new ThoughtfulView();
+
+
 
   var ExpressEmotionView = Backbone.View.extend({
     // collection: "EmotionsCollection",
     model: "EmotionDataModel",
     el: '.showemotion-wrap',
     render: function(){
-      $el.css("background-image", toString(giphyPlayUrl));
+      $(".showemotion-wrap").css("background-image", toString(gifPlayUrl));
+      console.log('i rendered')
     }
-
   });
 
-  var expressEmotionView = new ExpressEmotionView();
-  expressEmotionView.render();
+var expressEmotionView = new ExpressEmotionView();
+
+
+
 
 //From backbone doc
 //view.listenTo(model, 'change', view.render);
@@ -184,6 +423,8 @@ var MyRouter = Backbone.Router.extend({
 
   express: function(){
 
+      var expressEmotionView = new ExpressEmotionView();
+      expressEmotionView.render();
   }
 
 });
@@ -191,9 +432,10 @@ var MyRouter = Backbone.Router.extend({
 //   document.ready
 
 $(document).ready(function(){
-  var router = new MyRouter();
-  var anxiousView = new AnxiousView();
-  var boredView = new BoredView();
+  // var router = new MyRouter();
+  // var anxiousView = new AnxiousView();
+  // var boredView = new BoredView();
+
   Backbone.history.start();
   // console.log(emotionsAll.el);
 
